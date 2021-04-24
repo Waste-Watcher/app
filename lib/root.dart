fe6b8838fafe8d125d927703e0a7d3280aec3154
Fahim <fracturx@outlook.com>
2021-04-24 13:14:58 -0400

    Updated Project minSDK

---
+++ lib/root.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Root extends StatefulWidget {
+  @override
+  _RootState createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/route_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class RoutePage {
+  RoutePage({
+    @required this.title,
+    @required this.iconData,
+    @required this.navigatorKey,
+    @required this.page
+  });
+
+  final String title;
+  final IconData iconData;
+  final GlobalKey<NavigatorState> navigatorKey;
+  final Widget page;
+}
